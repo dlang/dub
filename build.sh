a08f415beaa00c101dcda3b8a69ec533c057dca2
Sönke Ludwig <sludwig@rejectedsoftware.com>
2013-02-22 11:41:12 +0100

    Using pkg-config if available to build on Posix.

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-rdmd --build-only -ofdub -g -debug -Isource -L-lcurl $* source/app.d
+LIBS=`pkg-config --libs libcurl 2>/dev/null || echo "-lcurl"`
+LIBS=`echo "$LIBS" | sed 's/^-L/-L-L/; s/ -L/ -L-L/g; s/^-l/-L-l/; s/ -l/ -L-l/g'`
+rdmd --build-only -ofdub -g -debug -Isource $LIBS $* source/app.d
