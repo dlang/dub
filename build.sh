9b50e599089bba28549db2dc28e16c18c488d835
jordisayol <g.sayol@yahoo.es>
2013-03-10 21:02:49 +0100

    Fix Linux building error

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-LIBS=`pkg-config --libs libcurl 2>/dev/null || echo "-lcurl"`
+LIBS=`pkg-config --libs libcurl 2>/dev/null || echo "-lcurl"`" -lphobos2"
 LIBS=`echo "$LIBS" | sed 's/^-L/-L-L/; s/ -L/ -L-L/g; s/^-l/-L-l/; s/ -l/ -L-l/g'`
 rdmd --build-only -ofdub -g -debug -w -property -Isource $LIBS $* source/app.d
