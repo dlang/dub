12d9d402eb8a3653702e8501f944dd239a80b702
Sönke Ludwig <sludwig@rejectedsoftware.com>
2013-03-22 11:46:40 +0100

    Improve wording of the post-build message.

--- build.cmd
+++ build.cmd
@@ -3,5 +3,6 @@
 echo Executing rdmd ...
 rdmd --force --build-only -ofbin\dub.exe -g -debug -w -property -Isource curl.lib %* source\app.d
 
-echo dub has been build. You propably want to add this to your PATH environment variable:
+echo DUB has been built. You probably also want to add the following entry to your
+echo PATH environment variable:
 echo %CD%\bin
--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ LIBS=`echo "$LIBS" | sed 's/^-L/-L-L/; s/ -L/ -L-L/g; s/^-l/-L-l/; s/ -l/ -L-l/g
 uname -a | grep "Ubuntu" > /dev/null && LIBS="-L--no-as-needed $LIBS"
 
 rdmd --build-only -ofbin/dub -g -debug -w -property -Isource $* $LIBS source/app.d
-echo dub has been build to bin/dub.
+echo DUB has been built as bin/dub
