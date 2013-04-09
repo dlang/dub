4b7b8d4fae99e2e51f0a17791d1266d1bccb1738
Sönke Ludwig <sludwig@rejectedsoftware.com>
2013-04-09 13:35:05 +0200

    Don't print build success after build errors.

--- build.cmd
+++ build.cmd
@@ -2,6 +2,7 @@
 
 echo Executing rdmd ...
 rdmd --force --build-only -ofbin\dub.exe -g -debug -w -property -Isource curl.lib %* source\app.d
+if errorlevel 1 exit /b 1
 
 echo DUB has been built. You probably also want to add the following entry to your
 echo PATH environment variable:
--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e
+
 LIBS=`pkg-config --libs libcurl 2>/dev/null || echo "-lcurl"`
 LIBS=`echo "$LIBS" | sed 's/^-L/-L-L/; s/ -L/ -L-L/g; s/^-l/-L-l/; s/ -l/ -L-l/g'`
 
