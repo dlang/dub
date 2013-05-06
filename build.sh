1dd2b637296f043afe6fceca665a7c4dfea5af8b
Sönke Ludwig <sludwig@rejectedsoftware.com>
2013-05-06 08:02:48 +0200

    Redo build script to directly call the compiler + limited compiler auto-detection.
    No rdmd is necessary anymore and the call to "uname -a" has been changed to
    "lsb_release -i" to detect the distribution more reliably. This needs to be
    properly fixed, though, by looking at the GCC version or by using a
    workaround that works on older _and_ newer versions.

--- build.sh
+++ build.sh
@@ -1,12 +1,30 @@
 #!/bin/sh
 set -e
 
+if [ "$DC" == "" ]; then
+	command -v gdmd >/dev/null 2>&1 && DC=gdmd || true
+	command -v ldmd2 >/dev/null 2>&1 && DC=ldmd2 || true
+	command -v dmd >/dev/null 2>&1 && DC=dmd || true
+fi
+
+if [ "$DC" == "" ]; then
+	echo >&2 "Failed to detect D compiler. Use DC=... to set a dmd compatible binary manually."
+	exit 1
+fi
+
 LIBS=`pkg-config --libs libcurl 2>/dev/null || echo "-lcurl"`
 LIBS=`echo "$LIBS" | sed 's/^-L/-L-L/; s/ -L/ -L-L/g; s/^-l/-L-l/; s/ -l/ -L-l/g'`
 
 # HACK to work around (r)dmd placing -lcurl before the object files - which is wrong if --as-needed is used
 # On newer Ubuntu versions this is the default, though
-uname -a | grep "Ubuntu" > /dev/null && LIBS="-L--no-as-needed $LIBS"
+if [ -f /etc/lsb-release ]; then
+	lsb_release -i | grep "Ubuntu" 2> /dev/null && LIBS="-L--no-as-needed $LIBS"
+fi
 
-rdmd --build-only -ofbin/dub -g -debug -w -property -Isource $* $LIBS source/app.d
-echo DUB has been built as bin/dub
+echo Running $DC...
+$DC -ofbin/dub -g -debug -w -property -Isource $* $LIBS @build-files.txt
+echo DUB has been built as bin/dub.
+echo
+echo You may want to run
+echo sudo ln -s $(pwd)/bin/dub /usr/local/bin
+echo now.
