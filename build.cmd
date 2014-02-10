07091430be8f3b6a26213b3d15d9d92f629464a8
Sönke Ludwig <sludwig@rejectedsoftware.com>
2014-02-10 21:17:20 +0100

    Don't use "echo off" to avoid influencing potential parent batch files.

--- build.cmd
+++ build.cmd
@@ -1,15 +1,14 @@
-@echo off
-if "%DC%"=="" set DC=dmd
+@if "%DC%"=="" set DC=dmd
 
-echo Generating version file...
-set GITVER=unknown
-for /f %%i in ('git describe') do set GITVER=%%i
-echo module dub.version_; enum dubVersion = "%GITVER%"; > source\dub\version_.d
+@echo Generating version file...
+@set GITVER=unknown
+@for /f %%i in ('git describe') do @set GITVER=%%i
+@echo module dub.version_; enum dubVersion = "%GITVER%"; > source\dub\version_.d
 
-echo Executing %DC%...
-%DC% -ofbin\dub.exe -g -debug -w -version=DubUseCurl -Isource curl.lib %* @build-files.txt
-if errorlevel 1 exit /b 1
+@echo Executing %DC%...
+@%DC% -ofbin\dub.exe -g -debug -w -version=DubUseCurl -Isource curl.lib %* @build-files.txt
+@if errorlevel 1 exit /b 1
 
-echo DUB has been built. You probably also want to add the following entry to your
-echo PATH environment variable:
-echo %CD%\bin
+@echo DUB has been built. You probably also want to add the following entry to your
+@echo PATH environment variable:
+@echo %CD%\bin
