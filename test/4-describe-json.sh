30727982dd0c1116aa36dee5a919d883d6550ad2
w0rp <devw0rp@gmail.com>
2015-04-28 00:03:54 +0100

    Get rid of jq checks for now, as Travis doesn't seem to support it.

--- test/4-describe-json.sh
+++ test/4-describe-json.sh
@@ -16,19 +16,3 @@ if ! $DUB describe --compiler=$COMPILER > "$temp_file"; then
     die 'Printing describe JSON failed!'
 fi
 
-declare -A expr_map
-
-expr_map[description]='A test describe project'
-expr_map[name]='describe-project'
-expr_map[targetType]='sourceLibrary'
-expr_map['authors[0]']='nobody'
-
-for expression in "${!expr_map[@]}"; do
-    expected="${expr_map[$expression]}"
-
-    actual=`jq --raw-output '.packages[0].'"$expression" "$temp_file"`
-
-    if [[ "$actual" != "$expected" ]]; then
-        die "The value for $expression was wrong in the describe output!"
-    fi
-done
