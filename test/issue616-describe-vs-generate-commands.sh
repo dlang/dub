2f4cdac058598acb258356cf615955ab27f9dab0
Nick Sabalausky <Abscissa@users.noreply.github.com>
2015-07-26 17:31:00 -0400

    Missed a couple changes in last commit.

--- test/issue616-describe-vs-generate-commands.sh
+++ test/issue616-describe-vs-generate-commands.sh
@@ -4,11 +4,9 @@ set -e -o pipefail
 cd "$CURR_DIR"/issue616-describe-vs-generate-commands
 
 temp_file=`mktemp`
-temp_file2=`mktemp`
 
 function cleanup {
     rm $temp_file
-    rm $temp_file2
 }
 
 trap cleanup EXIT
@@ -25,13 +23,6 @@ echo "$CURR_DIR/issue616-describe-vs-generate-commands/src/" >> "$expected_file"
 echo "$CURR_DIR/issue616-subpack/src/" >> "$expected_file"
 echo "$CURR_DIR/issue616-subsubpack/src/" >> "$expected_file"
 echo "issue616-describe-vs-generate-commands" >> "$expected_file"
-
-# Create the expected output file to compare stderr against.
-expected_file2="$CURR_DIR/expected-issue616-output2"
-echo "preGenerateCommands: DUB_PACKAGES_USED=issue616-describe-vs-generate-commands,issue616-subpack,issue616-subsubpack" > "$expected_file2"
-echo "$CURR_DIR/issue616-describe-vs-generate-commands/src/" >> "$expected_file2"
-echo "$CURR_DIR/issue616-subpack/src/" >> "$expected_file2"
-echo "$CURR_DIR/issue616-subsubpack/src/" >> "$expected_file2"
 
 if ! diff "$expected_file" "$temp_file"; then
     die 'The stdout output did not match the expected output!'
