075899dfa340debcabec39703364dbe5b625d232
Guillaume Piolat <guillaume.piolat@gmail.com>
2015-09-28 16:44:59 +0200

    Remove unneeded export

--- build.sh
+++ build.sh
@@ -39,7 +39,7 @@ echo "module dub.version_;" > source/dub/version_.d
 echo "enum dubVersion = \"$GITVER\";" >> source/dub/version_.d
 
 # For OSX compatibility >= 10.6
-export MACOSX_DEPLOYMENT_TARGET=10.6
+MACOSX_DEPLOYMENT_TARGET=10.6
 
 echo Running $DMD...
 $DMD -ofbin/dub -w -version=DubUseCurl -Isource $* $LIBS @build-files.txt
