2d1df295b396e2b7a000ab5f82afde8177a3a841
Sönke Ludwig <sludwig@rejectedsoftware.com>
2015-12-11 18:37:01 +0100

    Fix rendundant architecture flag test.

--- test/issue346-redundant-flags.sh
+++ test/issue346-redundant-flags.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd ${CURR_DIR}/issue346-redundant-flags
-${DUB} build --bare --force --compiler=${DC} -a x86 main | grep -e "-m32 -m32" 2>&1 && exit 1 || exit 0
+${DUB} build --bare --force --compiler=${DC} -a x86_64 -v main 2>&1 | grep -e "-m64 -m64" -c && exit 1 || exit 0
