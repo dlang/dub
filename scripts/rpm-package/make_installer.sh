89dc47595a9da43a9489706479bb1a6dd30c5d25
Sönke Ludwig <sludwig@rejectedsoftware.com>
2015-12-19 15:49:48 +0100

    Fix the RPM package build script.

--- scripts/rpm-package/make_installer.sh
+++ scripts/rpm-package/make_installer.sh
@@ -4,7 +4,7 @@ cd ../../
 DUB_PATH=`pwd`
 #rm -f ~/rpmbuild/SOURCES/dub.tar.gz
 #tar -pczf ~/rpmbuild/SOURCES/dub.tar.gz source build-files.txt build.sh LICENSE*
-cd installer/rpm/
+cd scripts/rpm-package/
 for i in $(git describe | tr "-" "\n"); do
 	if [ "$VER" == "" ]; then
 		VER=${i:1}
