90299db446ee52f7ecaebb6c9dec4e8ef6999479
Sönke Ludwig <sludwig@rejectedsoftware.com>
2016-06-07 08:55:21 +0200

    Pass all remaining args to the script for "dub [file].d" invocations. Fixes #856.

--- test/issue103-single-file-package.d
+++ test/issue103-single-file-package.d
@@ -4,8 +4,9 @@
 +/
 module hello;
 
-void main()
+void main(string[] args)
 {
 	import std.stdio : writeln;
+	assert(args.length == 4 && args[1 .. 4] == ["foo", "--", "bar"]);
 	writeln("Hello, World!");
 }
--- test/issue103-single-file-package.sh
+++ test/issue103-single-file-package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 cd ${CURR_DIR}
-./issue103-single-file-package.d
+./issue103-single-file-package.d foo -- bar
 ${DUB} run --single issue103-single-file-package-json.d --compiler=${DC}
 ${DUB} issue103-single-file-package-w-dep.d --compiler=${DC}
