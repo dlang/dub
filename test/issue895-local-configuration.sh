945da964fcafe9ec98a662ef77ed9f48dbd67b4d
Sönke Ludwig <sludwig@rejectedsoftware.com>
2016-07-13 11:35:03 +0200

    Add test for issue #895.

---
+++ test/issue895-local-configuration.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+cd ${CURR_DIR}
+mkdir ../etc
+mkdir ../etc/dub
+echo "{\"defaultCompiler\": \"foo\"}" > ../etc/dub/settings.json
+
+if [ -e /var/lib/dub/settings.json ]; then
+	echo "Found existing system wide DUB configuration. Aborting."
+	exit 1
+fi
+
+if [ -e ~/.dub/settings.json ]; then
+	echo "Found existing user wide DUB configuration. Aborting."
+	exit 1
+fi
+
+if ! ${DUB} describe --single issue103-single-file-package.d 2>&1 | grep -e "Unknown compiler: foo" -c > /dev/null; then
+	rm -r ../etc
+	echo "DUB didn't find the local configuration"
+	exit 1
+fi
+
+rm -r ../etc
