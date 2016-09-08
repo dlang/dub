04a9caee088646c5f7de315d7c431ddc80537d36
Sönke Ludwig <sludwig@rejectedsoftware.com>
2016-09-08 13:52:45 +0200

    Add test for #923.

---
+++ test/issue923-subpackage-deps.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd ${CURR_DIR}/issue923-subpackage-deps
+rm -rf main/.dub
+rm -rf a/.dub
+rm -rf b/.dub
+rm -f main/dub.selections.json
+${DUB} build --bare --compiler=${DC} main || exit 1
+
+
+if ! grep -c -e \"b\" main/dub.selections.json; then
+	echo "Dependency b not resolved."
+	exit 1
+fi
