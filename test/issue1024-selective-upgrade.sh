a33edaf9e0fd402f695e8262a236e76896d12472
Sönke Ludwig <sludwig@rejectedsoftware.com>
2016-12-27 17:20:25 +0100

    Add test for selective upgrades.

---
+++ test/issue1024-selective-upgrade.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd ${CURR_DIR}/issue1024-selective-upgrade
+echo "{\"fileVersion\": 1,\"versions\": {\"a\": \"1.0.0\", \"b\": \"1.0.0\"}}" > main/dub.selections.json
+$DUB upgrade --bare --root=main a || exit 1
+
+if ! grep -c -e "\"a\": \"1.0.1\"" main/dub.selections.json; then
+	echo "Specified dependency was not upgraded."
+	exit 1
+fi
+
+if grep -c -e "\"b\": \"1.0.1\"" main/dub.selections.json; then
+	echo "Non-specified dependency got upgraded."
+	exit 1
+fi
