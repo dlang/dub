0cdbda0df8fe43c3052e425aa6555660a922282c
Sönke Ludwig <sludwig@rejectedsoftware.com>
2017-12-01 14:00:11 +0100

    Add pure/nogc annotations to the SemVer module.

--- source/dub/semver.d
+++ source/dub/semver.d
@@ -26,7 +26,7 @@ import std.conv;
 	Validates a version string according to the SemVer specification.
 */
 bool isValidVersion(string ver)
-{
+pure @nogc {
 	// NOTE: this is not by spec, but to ensure sane input
 	if (ver.length > 256) return false;
 
@@ -102,7 +102,7 @@ unittest {
 /**
 	Determines if a given valid SemVer version has a pre-release suffix.
 */
-bool isPreReleaseVersion(string ver)
+bool isPreReleaseVersion(string ver) pure @nogc
 in { assert(isValidVersion(ver)); }
 body {
 	foreach (i; 0 .. 2) {
@@ -136,7 +136,7 @@ unittest {
 		equal, and a positive number otherwise.
 */
 int compareVersions(string a, string b)
-{
+pure @nogc {
 	// compare a.b.c numerically
 	if (auto ret = compareNumber(a, b)) return ret;
 	assert(a[0] == '.' && b[0] == '.');
@@ -225,7 +225,8 @@ unittest {
 
 	See_Also: `expandVersion`
 */
-string bumpVersion(string ver) {
+string bumpVersion(string ver)
+pure {
 	// Cut off metadata and prerelease information.
 	auto mi = ver.indexOfAny("+-");
 	if (mi > 0) ver = ver[0..mi];
@@ -258,7 +259,8 @@ unittest {
 
 	See_Also: `bumpVersion`
 */
-string expandVersion(string ver) {
+string expandVersion(string ver)
+pure {
 	auto mi = ver.indexOfAny("+-");
 	auto sub = "";
 	if (mi > 0) {
@@ -282,7 +284,7 @@ unittest {
 }
 
 private int compareIdentifier(ref string a, ref string b)
-{
+pure @nogc {
 	bool anumber = true;
 	bool bnumber = true;
 	bool aempty = true, bempty = true;
@@ -312,7 +314,7 @@ private int compareIdentifier(ref string a, ref string b)
 }
 
 private int compareNumber(ref string a, ref string b)
-{
+pure @nogc {
 	int res = 0;
 	while (true) {
 		if (a[0] != b[0] && res == 0) res = a[0] - b[0];
@@ -325,7 +327,7 @@ private int compareNumber(ref string a, ref string b)
 }
 
 private bool isValidIdentifierChain(string str, bool allow_leading_zeros = false)
-{
+pure @nogc {
 	if (str.length == 0) return false;
 	while (str.length) {
 		auto end = str.indexOf('.');
@@ -338,7 +340,7 @@ private bool isValidIdentifierChain(string str, bool allow_leading_zeros = false
 }
 
 private bool isValidIdentifier(string str, bool allow_leading_zeros = false)
-{
+pure @nogc {
 	if (str.length < 1) return false;
 
 	bool numeric = true;
@@ -361,7 +363,7 @@ private bool isValidIdentifier(string str, bool allow_leading_zeros = false)
 }
 
 private bool isValidNumber(string str)
-{
+pure @nogc {
 	if (str.length < 1) return false;
 	foreach (ch; str)
 		if (ch < '0' || ch > '9')
@@ -374,7 +376,7 @@ private bool isValidNumber(string str)
 }
 
 private sizediff_t indexOfAny(string str, in char[] chars)
-{
+pure @nogc {
 	sizediff_t ret = -1;
 	foreach (ch; chars) {
 		auto idx = str.indexOf(ch);
