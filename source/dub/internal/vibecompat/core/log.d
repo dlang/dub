262c6604794f0f9a1490c9b0e48f691555f56370
Giacomo De Lazzari <gdelazzari@aol.com>
2018-06-11 15:30:52 +0200

    Re-added dub.internal.vibecompat.core.log as deprecated
    I simply aliases the new dub.logging module, printing a deprecation
    message while compiling

---
+++ source/dub/internal/vibecompat/core/log.d
@@ -0,0 +1,14 @@
+/**	+deprecated("Please use dub.logging")
+	This module contained logging functions, but has been replaced by dub.logging
+  which provides colored output and other facilities.
+
+  Copyright: © 2012 rejectedsoftware e.K., © 2018 Giacomo De Lazzari
+	License: Subject to the terms of the MIT license, as written in the included LICENSE.txt file.
+	Authors: Sönke Ludwig, Giacomo De Lazzari
+*/
+
+deprecated("Please use dub.logging")
+
+module dub.internal.vibecompat.core.log;
+
+public import dub.logging;
