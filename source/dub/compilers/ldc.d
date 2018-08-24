ec2b6e35feab9f8e539be554c5cc9a515a3be10f
Anton Fediushin <fediushin.anton@yandex.com>
2018-08-24 20:44:39 +0200

    Fix dynamicLibrary extensions on OSX. Fixes #1228

--- source/dub/compilers/ldc.d
+++ source/dub/compilers/ldc.d
@@ -167,6 +167,8 @@ class LDCCompiler : Compiler {
 			case TargetType.dynamicLibrary:
 				if (platform.platform.canFind("windows"))
 					return settings.targetName ~ ".dll";
+				else if (platform.platform.canFind("osx"))
+					return "lib" ~ settings.targetName ~ ".dylib";
 				else return "lib" ~ settings.targetName ~ ".so";
 			case TargetType.object:
 				if (platform.platform.canFind("windows"))
