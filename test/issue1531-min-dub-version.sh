59dac6e6996028641a3a7f83bfa728527a7103a6
Remi Thebault <remi.thebault@gmail.com>
2018-09-30 17:03:19 +0200

    implement test 1531 from stdin

--- test/issue1531-min-dub-version.sh
+++ test/issue1531-min-dub-version.sh
@@ -3,17 +3,16 @@ set -e
 
 . $(dirname "${BASH_SOURCE[0]}")/common.sh
 
-DIR1531=${CURR_DIR}/issue1531-min-dub-version
+cat << EOF | $DUB - || die "Did not pass minDubVersion \"1.0\""
+/+ dub.sdl:
+    minDubVersion "1.0"
++/
+void main() {}
+EOF
 
-rm -rf $DIR1531/.dub/
-rm -rf $DIR1531/test-application*
-
-sed -i 's/^minDubVersion ".*"$/minDubVersion "1.0"/' ${DIR1531}/dub.sdl
-${DUB} run --root ${DIR1531} || die "Did not pass minDubVersion \"1.0\""
-
-sed -i 's/^minDubVersion ".*"$/minDubVersion "99.0"/' $DIR1531/dub.sdl
-! ${DUB} run --root ${DIR1531} || die "Did pass minDubVersion \"99.0\"!"
-
-sed -i 's/^minDubVersion ".*"$/minDubVersion "1.0"/' $DIR1531/dub.sdl
-rm -rf $DIR1531/.dub/
-rm -rf $DIR1531/test-application*
+! cat << EOF | $DUB - || die "Did pass minDubVersion \"99.0\""
+/+ dub.sdl:
+    minDubVersion "99.0"
++/
+void main() {}
+EOF
--- test/issue1531-min-dub-version/source/app.d
+++
@@ -1,3 +0,0 @@
-module app;
-
-void main() {}
