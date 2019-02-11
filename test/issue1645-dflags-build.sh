b072884037e4f9e407f297dcee11f0c58449355c
Victor Porton <porton@narod.ru>
2019-02-11 16:13:16 +0200

    bug in test

--- test/issue1645-dflags-build.sh
+++ test/issue1645-dflags-build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+set -e
+
 # If DFLAGS are not processed, dub for library would fail
 DFLAGS="-w" $DUB --root="$CURR_DIR"/1-staticLib-simple --build=plain
 if DFLAGS="-asfdsf" $DUB --root="$CURR_DIR"/1-staticLib-simple --build=plain 2>/dev/null; then
-  echo "Should not accept this DFLAGS"
+  echo "Should not accept this DFLAGS";
+  false # fail
 fi
 $DUB --root="$CURR_DIR"/1-staticLib-simple --build=plain --build=plain
