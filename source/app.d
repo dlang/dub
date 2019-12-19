b5c5fea53925b97d3bc7e4fb52d666245de2ea56
Geod24 <pro.mathias.lang@gmail.com>
2019-12-20 03:56:52 +0900

    Disable parallel marking within DUB

--- source/app.d
+++ source/app.d
@@ -9,6 +9,18 @@ module app;
 
 import dub.commandline;
 
+/**
+ * Workaround https://github.com/dlang/dub/issues/1812
+ *
+ * On Linux, a segmentation fault happens when dub is compiled with a recent
+ * compiler. While not confirmed, the logs seem to point to parallel marking
+ * done by the GC. Hence this disables it.
+ *
+ * https://dlang.org/changelog/2.087.0.html#gc_parallel
+ */
+static if (__VERSION__ >= 2087)
+    extern(C) __gshared string[] rt_options = [ "gcopt=parallel:0" ];
+
 int main(string[] args)
 {
 	return runDubCommandLine(args);
