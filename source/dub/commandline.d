305a61ef8debcfe378dde89d961b8c09d66219d6
Shigeki Karita <shigekikarita@gmail.com>
2019-12-27 15:23:04 +0900

    add flush before reading input

--- source/dub/commandline.d
+++ source/dub/commandline.d
@@ -521,6 +521,8 @@ class InitCommand : Command {
 		static string input(string caption, string default_value)
 		{
 			writef("%s [%s]: ", caption, default_value);
+			// Msys2 and mingw on Windows require this flush()
+			version (Windows) stdout.flush();
 			auto inp = readln();
 			return inp.length > 1 ? inp[0 .. $-1] : default_value;
 		}
