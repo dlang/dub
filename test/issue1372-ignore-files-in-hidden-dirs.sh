5aef26853cd092190a7787ed52e79de6d8a3a792
Samathy Barratt <samathy@sbarratt.co.uk>
2020-01-05 19:55:45 +0000

    Add test for explicitly included source files in hidden dirs

--- test/issue1372-ignore-files-in-hidden-dirs.sh
+++ test/issue1372-ignore-files-in-hidden-dirs.sh
@@ -2,4 +2,15 @@
 
 cd ${CURR_DIR}/issue1372-ignore-files-in-hidden-dirs/
 
+echo "Compile and ignore hidden directories"
+rm dub.json
+ln -s dub_json_no_hidden.json dub.json
 ${DUB} build --force
+
+rm dub.json
+
+echo "Compile and explcitly include file in hidden directories"
+ln -s dub_json_hidden.json dub.json
+${DUB} build --force
+
+rm dub.json
