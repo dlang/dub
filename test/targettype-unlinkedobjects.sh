410d34ae7893948cf2d0a64fc8f5c26e413a1b89
Jan Jurzitza <gh@webfreak.org>
2020-02-16 20:18:36 +0000

    Log actual gotten object files for error finding

--- test/targettype-unlinkedobjects.sh
+++ test/targettype-unlinkedobjects.sh
@@ -12,11 +12,11 @@ ls obj > actual.txt
 # LDC currently names the files the full module name and DMD just the filename
 if [[ $DC == *ldc* ]] || [[ $DC == *ldmd* ]]; then
 	if ! diff expected.ldc.txt actual.txt; then
-		die "didn't get expected object files"
+		die "didn't get expected object files, got instead: $(<actual.txt)"
 	fi
 elif [[ $DC == *dmd* ]]; then
 	if ! diff expected.dmd.txt actual.txt; then
-		die "didn't get expected object files"
+		die "didn't get expected object files, got instead: $(<actual.txt)"
 	fi
 # GDC unsupported
 fi
