d51ae6b8bccea6d91c0c18e16c5326d83de56b44
Geod24 <pro.mathias.lang@gmail.com>
2020-05-18 17:48:41 +0900

    testsuite: Make test for issue1091 re-runnable
    After the initial run, one couldn't run this test again as the expected message
    'building configuration' would not be printed anymore.

--- test/issue1091-bogus-rebuild.sh
+++ test/issue1091-bogus-rebuild.sh
@@ -4,5 +4,6 @@
 
 cd ${CURR_DIR}/1-exec-simple
 rm -f dub.selections.json
+${DUB} clean
 ${DUB} build --compiler=${DC} 2>&1 | grep -e 'building configuration' -c
 ${DUB} build --compiler=${DC} 2>&1 | { ! grep -e 'building configuration' -c; }
