4ab453b988cdf79e2d9a4a2c04dbde75391d4687
drug007 <drug2004@bk.ru>
2020-11-13 03:05:56 +0300

    Appveyor + Travis

--- scripts/ci/travis.sh
+++ scripts/ci/travis.sh
@@ -28,6 +28,7 @@ if [ "$COVERAGE" = true ]; then
     bash codecov.sh
 else
     ./build.d
+    `pwd`/bin/dub --single scripts/ci/hash_dependent_build.d
     DUB=`pwd`/bin/dub DC=${DC} test/run-unittest.sh
 fi
 
