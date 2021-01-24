a0737ccb686eb9e057dbe588b88454dc1d960400
Bastiaan Veelo <Bastiaan@SARC.nl>
2021-01-24 22:30:46 +0100

    Remove unused imports.

--- source/dub/compilers/compiler.d
+++ source/dub/compilers/compiler.d
@@ -11,15 +11,12 @@ public import dub.compilers.buildsettings;
 public import dub.dependency : Dependency;
 public import dub.platform : BuildPlatform, matchesSpecification;
 
-import dub.internal.vibecompat.core.file;
 import dub.internal.vibecompat.core.log;
-import dub.internal.vibecompat.data.json;
 import dub.internal.vibecompat.inet.path;
 import dub.recipe.packagerecipe : ToolchainRequirements;
 
 import std.algorithm;
 import std.array;
-import std.conv;
 import std.exception;
 import std.process;
 import std.typecons : Flag;
