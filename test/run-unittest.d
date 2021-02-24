0ef66d0e5c5b7baab63edc36e781893778cd3112
drug007 <drug2004@bk.ru>
2021-02-24 11:21:09 +0300

    Add common package

--- test/run-unittest.d
+++ test/run-unittest.d
@@ -2,44 +2,11 @@
 /+dub.sdl:
 	name: run_unittest
 	targetName: run-unittest
+	dependency "common" path="./common"
 +/
 module run_unittest;
 
-/// Name of the log file
-enum logFile = "test.log";
-
-/// has true if some test fails
-bool any_errors = false;
-
-/// prints (non error) message to standard output and log file
-void log(Args...)(Args args)
-	if (Args.length)
-{
-	import std.conv : text;
-	import std.stdio : File, stdout;
-
-	const str = text("[INFO] ", args);
-	version(Windows) stdout.writeln(str);
-	else stdout.writeln("\033[0;33m", str, "\033[0m");
-	stdout.flush;
-	File(logFile, "a").writeln(str);
-}
-
-/// prints error message to standard error stream and log file
-/// and set any_errors var to true value to indicate that some
-/// test fails
-void logError(Args...)(Args args)
-{
-	import std.conv : text;
-	import std.stdio : File, stderr;
-
-	const str = text("[ERROR] ", args);
-	version(Windows) stderr.writeln(str);
-	else stderr.writeln("\033[0;31m", str, "\033[0m");
-	stderr.flush;
-	File(logFile, "a").writeln(str);
-	any_errors = true;
-}
+import common;
 
 int main(string[] args)
 {
