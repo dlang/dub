a72d2e5acbe2f289800d5eab6d5299a27fdc05ef
Ömer Faruk Irmak <omerfirmak@gmail.com>
2021-10-14 16:54:21 +0300

    Fix build setting inheritance
    First inherit upward then force the dependency build settings
    downwards. Fixes the dependency build settings leaking upwards.
    
    Fixes #2176
    Fixes #2177

--- test/depen-build-settings/source/app.d
+++ test/depen-build-settings/source/app.d
@@ -3,10 +3,13 @@ import std.file;
 
 extern (C) void depend1_func();
 
+version (must_be_defined) static assert(0, "Expected must_be_defined not to be set");
+
 void main()
 {
 	writeln("Edit source/app.d to start your project.");
 	depend1_func();
+	assert(!exists("depen-build-settings.json"));
 	assert(exists("depend2.json"));
 	assert(exists("depend.json"));
 }
