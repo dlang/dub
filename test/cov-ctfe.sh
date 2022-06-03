36ecf328f33028e78f658177e8e5c87f1f3bf20d
Per Nordlöw <per.nordlow@gmail.com>
2022-06-04 01:10:41 +0200

    Don’t call writeln in generated code when measuring coverage and indent generated code

--- test/cov-ctfe.sh
+++ test/cov-ctfe.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 DIR=$(dirname "${BASH_SOURCE[0]}")
-
 . "$DIR"/common.sh
-
-DFLAGS='-cov=100' "$DUB" run --root "$DIR"/cov-ctfe --build=cov-ctfe
+"$DUB" run --root "$DIR"/cov-ctfe --build=cov-ctfe
--- test/unittest-cov-ctfe.sh
+++ test/unittest-cov-ctfe.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 DIR=$(dirname "${BASH_SOURCE[0]}")
-
 . "$DIR"/common.sh
-
-DFLAGS='-cov=100' "$DUB" test --root "$DIR"/unittest-cov-ctfe --build=unittest-cov-ctfe
+"$DUB" test --root "$DIR"/unittest-cov-ctfe --build=unittest-cov-ctfe
