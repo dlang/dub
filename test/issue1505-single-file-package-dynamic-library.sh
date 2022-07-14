287894423066a83f07db1270368c6ecc34273a3e
Geod24 <pro.mathias.lang@gmail.com>
2022-07-14 15:16:10 +0200

    CI: Fix script for issue1505
    The syntax was incorrect but the script was happily completing.

--- test/issue1505-single-file-package-dynamic-library.sh
+++ test/issue1505-single-file-package-dynamic-library.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
+set -eux -o pipefail
+
 . $(dirname "${BASH_SOURCE[0]}")/common.sh
 cd ${CURR_DIR}
+
 rm -f libsingle-file-test-dynamic-library.{so,dylib}
 rm -f single-file-test-dynamic-library.dll
 
 ${DUB} build --single issue1505-single-file-package-dynamic-library.d
-if [ ! -f libsingle-file-test-dynamic-library.{so,dylib} ] && [ ! -f single-file-test-dynamic-library.dll ]; then
+if [[ ! -f libsingle-file-test-dynamic-library.{so,dylib} ]] && [[ ! -f single-file-test-dynamic-library.dll ]]; then
 	die $LINENO 'Normal invocation did not produce a dynamic library in the current directory'
 fi
 rm -f libsingle-file-test-dynamic-library.{so,dylib}
