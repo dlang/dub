ca78b07b0bfed153f8bbb8fabb97395f9562373c
Bastiaan Veelo <Bastiaan@SARC.nl>
2022-07-20 17:37:33 +0200

    Don't require PERL regex in unittest.

--- test/colored-output.sh
+++ test/colored-output.sh
@@ -5,13 +5,13 @@
 cd ${CURR_DIR}/1-exec-simple
 
 # Test --colors=off disabling colors correctly
-${DUB} build --colors=off --compiler=${DC} 2>&1 | { ! grep -P '\e\[' -c; }
+${DUB} build --colors=off --compiler=${DC} 2>&1 | { ! \grep $'^\x1b\[' -c; }
 
-# Test --colors=auto detecting no TTY
-${DUB} build --colors=auto --compiler=${DC} 2>&1 | { ! grep -P '\e\[' -c; }
+# Test --colors=automatic detecting no TTY
+${DUB} build --colors=automatic --compiler=${DC} 2>&1 | { ! \grep $'^\x1b\[' -c; }
 
-# Test no --colors= option defaulting to auto
-${DUB} build --compiler=${DC} 2>&1 | { ! grep -P '\e\[' -c; }
+# Test no --colors= option defaulting to automatic
+${DUB} build --compiler=${DC} 2>&1 | { ! \grep $'^\x1b\[' -c; }
 
 # Test --colors=on enabling colors in any case
-${DUB} build --colors=on --compiler=${DC} 2>&1 | grep -P '\e\[' -c
+${DUB} build --colors=on --compiler=${DC} 2>&1 | \grep $'^\x1b\[' -c
