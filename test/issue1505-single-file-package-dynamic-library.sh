fe782928736ddd5d26e883252a8a6d2c09b37a3a
WebFreak001 <gh@webfreak.org>
2022-07-28 18:05:25 +0200

    fix file path expansion not done in test script

--- test/issue1505-single-file-package-dynamic-library.sh
+++ test/issue1505-single-file-package-dynamic-library.sh
@@ -8,7 +8,9 @@ rm -f libsingle-file-test-dynamic-library.{so,dylib}
 rm -f single-file-test-dynamic-library.dll
 
 ${DUB} build --single issue1505-single-file-package-dynamic-library.d
-if [[ ! -f libsingle-file-test-dynamic-library.{so,dylib} ]] && [[ ! -f single-file-test-dynamic-library.dll ]]; then
+if [[ ! -f libsingle-file-test-dynamic-library.so ]] \
+&& [[ ! -f libsingle-file-test-dynamic-library.dylib ]] \
+&& [[ ! -f single-file-test-dynamic-library.dll ]]; then
 	die $LINENO 'Normal invocation did not produce a dynamic library in the current directory'
 fi
 rm -f libsingle-file-test-dynamic-library.{so,dylib}
