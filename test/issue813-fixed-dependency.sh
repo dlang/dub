4babcf86a1660736b5e02bda60e774b1e1569d82
richard (rikki) andrew cattermole <richard@cattermole.co.nz>
2022-12-11 21:27:15 +1300

    First lets verify that we actually break the test suite

--- test/issue813-fixed-dependency.sh
+++ test/issue813-fixed-dependency.sh
@@ -5,4 +5,19 @@ cd ${CURR_DIR}/issue813-fixed-dependency
 rm -rf main/.dub
 rm -rf sub/.dub
 rm -rf sub/sub/.dub
+
+# verify that we can build sub without add-local
+${DUB} build --bare --compiler=${DC} sub
+
+rm -rf sub/.dub
+rm -rf sub/sub/.dub
+
+# verify that we can build sub with add-local
+${DUB} add-local sub
+${DUB} build --bare --compiler=${DC} sub
+
+rm -rf sub/.dub
+rm -rf sub/sub/.dub
+
+# verify that we can build main application
 ${DUB} build --bare --compiler=${DC} main
