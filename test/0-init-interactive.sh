30b12f27901f39e07928e3a4c23e2b5daf46211a
WebFreak001 <gh@webfreak.org>
2023-01-11 22:53:24 +0100

    add test for described license

--- test/0-init-interactive.sh
+++ test/0-init-interactive.sh
@@ -39,6 +39,8 @@ runTest '2\ntest\ndesc\nauthor\ngpl\ncopy\n\n' 0-init-interactive.dub.json
 runTest '\ntest\ndesc\nauthor\ngpl\ncopy\n\n' 0-init-interactive.dub.json
 # select license
 runTest '1\ntest\ndesc\nauthor\n6\n3\ncopy\n\n' 0-init-interactive.license_gpl3.dub.sdl
+# select license (with description)
+runTest '1\ntest\ndesc\nauthor\n9\n3\ncopy\n\n' 0-init-interactive.license_mpl2.dub.sdl
 # select license out of bounds
 runTest '1\ntest\ndesc\nauthor\n21\n6\n3\ncopy\n\n' 0-init-interactive.license_gpl3.dub.sdl
 # default license
