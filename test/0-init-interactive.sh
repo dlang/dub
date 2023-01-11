279e32f71e3e5f33c76a18ad57b38a4773165c5b
WebFreak001 <gh@webfreak.org>
2023-01-11 21:15:11 +0100

    fix tests

--- test/0-init-interactive.sh
+++ test/0-init-interactive.sh
@@ -24,22 +24,22 @@ function runTest {
 }
 
 # sdl package format
-runTest '0\ntest\ndesc\nauthor\ngpl\ncopy\n\n' 0-init-interactive.dub.sdl
+runTest '1\ntest\ndesc\nauthor\ngpl\ncopy\n\n' 0-init-interactive.dub.sdl
 # select package format out of bounds
-runTest '3\n0\ntest\ndesc\nauthor\ngpl\ncopy\n\n' 0-init-interactive.dub.sdl
+runTest '3\n1\ntest\ndesc\nauthor\ngpl\ncopy\n\n' 0-init-interactive.dub.sdl
 # select package format not numeric, but in list
 runTest 'sdl\ntest\ndesc\nauthor\ngpl\ncopy\n\n' 0-init-interactive.dub.sdl
 # selected value not numeric and not in list
-runTest 'sdlf\n0\ntest\ndesc\nauthor\ngpl\ncopy\n\n' 0-init-interactive.dub.sdl
+runTest 'sdlf\n1\ntest\ndesc\nauthor\ngpl\ncopy\n\n' 0-init-interactive.dub.sdl
 # default name
-runTest '0\n\ndesc\nauthor\ngpl\ncopy\n\n' 0-init-interactive.default_name.dub.sdl
+runTest '1\n\ndesc\nauthor\ngpl\ncopy\n\n' 0-init-interactive.default_name.dub.sdl
 # json package format
-runTest '1\ntest\ndesc\nauthor\ngpl\ncopy\n\n' 0-init-interactive.dub.json
+runTest '2\ntest\ndesc\nauthor\ngpl\ncopy\n\n' 0-init-interactive.dub.json
 # default package format
 runTest '\ntest\ndesc\nauthor\ngpl\ncopy\n\n' 0-init-interactive.dub.json
 # select license
-runTest '0\ntest\ndesc\nauthor\n3\ncopy\n\n' 0-init-interactive.license_gpl3.dub.sdl
+runTest '1\ntest\ndesc\nauthor\n4\ncopy\n\n' 0-init-interactive.license_gpl3.dub.sdl
 # select license out of bounds
-runTest '0\ntest\ndesc\nauthor\n21\n3\ncopy\n\n' 0-init-interactive.license_gpl3.dub.sdl
+runTest '1\ntest\ndesc\nauthor\n21\n4\ncopy\n\n' 0-init-interactive.license_gpl3.dub.sdl
 # default license
-runTest '0\ntest\ndesc\nauthor\n\ncopy\n\n' 0-init-interactive.license_proprietary.dub.sdl
+runTest '1\ntest\ndesc\nauthor\n\ncopy\n\n' 0-init-interactive.license_proprietary.dub.sdl
