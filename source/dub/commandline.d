720c5d6792888470bc709e2a54325a1e7f52a193
elvodqa <emircengiz21@gmail.com>
2023-02-13 14:55:46 +0300

    Remove usage info

--- source/dub/commandline.d
+++ source/dub/commandline.d
@@ -473,13 +473,12 @@ int runDubCommandLine(string[] args)
 			foreach (Command command; key.commands)
 			{
 				if (levenshteinDistance(command_name_argument.value, command.name) < 4) {
-					logInfo("Did you mean '%s'?", command.name);
+					logInfo("Did you mean '%s?", command.name);
 				}
 			}
 		}
 
 		writeln();
-		showHelp(handler.commandGroups, common_args);
 		return 1;
 	}
 
