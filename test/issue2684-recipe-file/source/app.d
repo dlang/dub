07b24d0dd93bf3492ce4ed7e69c989766d00b8b6
Hipreme <msnmancini@hotmail.com>
2023-08-10 19:14:50 -0300

    Added: Tests

---
+++ test/issue2684-recipe-file/source/app.d
@@ -0,0 +1,10 @@
+module app;
+import std.stdio;
+
+void main()
+{
+	writeln("This was built using dub.json.
+Try using the other configuration by calling dub with:
+dub --recipe=dubWithAnotherSource.json
+");
+}
