d3df75683aade5a8a8c94e9e966267022054f357
Martin Kinkelin <noone@nowhere.com>
2023-08-25 16:55:23 +0200

    [avoid setting TMPDIR in bash test script; seems to affect invoked processes somehow]
    On macOS at least, according to CI - LDC newly creates the object
    files in a unique temp dir, and TMPDIR is used as parent dir. The
    directory doesn't exist yet for the initial build.

--- test/prefer-add-path-packages.sh
+++ test/prefer-add-path-packages.sh
@@ -2,13 +2,13 @@
 
 . $(dirname ${BASH_SOURCE[0]})/common.sh
 
-TMPDIR=$CURR_DIR/tmp-add-path
+TMP_DIR=$CURR_DIR/tmp-add-path
 
 PACK_PATH="$CURR_DIR"/issue2262-exact-cached-version-match
 
 # make sure that there are no left-over selections files or temp directory
 rm -f $PACK_PATH/dub.selections.json
-rm -rf $TMPDIR
+rm -rf $TMP_DIR
 
 # make sure that there are no cached versions of the dependency
 $DUB remove gitcompatibledubpackage@* -n || true
@@ -19,26 +19,26 @@ if ! ${DUB} build --root $PACK_PATH | grep "gitcompatibledubpackage 1\.0\.4:"; t
 fi
 
 # clone gitcompatibledubpackage and check out 1.0.4+commit.2.ccb31bf
-mkdir $TMPDIR
-$DUB add-path $TMPDIR
-cd $TMPDIR
+mkdir $TMP_DIR
+$DUB add-path $TMP_DIR
+cd $TMP_DIR
 git clone https://github.com/dlang-community/gitcompatibledubpackage.git
 cd gitcompatibledubpackage
 git checkout -q ccb31bf6a655437176ec02e04c2305a8c7c90d67
 cd ../..
 
 if ! $DUB list | grep "gitcompatibledubpackage 1\.0\.4+commit.2.gccb31bf"; then
-    $DUB remove-path $TMPDIR
+    $DUB remove-path $TMP_DIR
     die $LINENO 'Cloned package was not found in search path'
 fi
 
 # should pick up the cloned package instead of the cached one now
 if ! ${DUB} build --root $PACK_PATH | grep "gitcompatibledubpackage 1\.0\.4+commit.2.gccb31bf:"; then
-    $DUB remove-path $TMPDIR
+    $DUB remove-path $TMP_DIR
     die $LINENO 'Did not pick up the add-path package.'
 fi
 
 # clean up
-$DUB remove-path $TMPDIR
+$DUB remove-path $TMP_DIR
 rm -f $PACK_PATH/dub.selections.json
-rm -rf $TMPDIR
+rm -rf $TMP_DIR
