1f7a6dd2fef1cafffabb356b90791c030b2e2291
Mathias Lang <mlang@symmetryinvestments.com>
2023-12-26 21:39:55 +0100

    Allow dependency injection on the PackageSupplier
    In order to make the `Dub` class properly unittest-able,
    we need to be able to use a non-default `PackageSupplier`,
    in particular one that would not hit the registry or the filesystem.
    This can be achieved by moving a free function to a method and having
    derived class override it, as shown in this commit.

--- source/dub/test/base.d
+++ source/dub/test/base.d
@@ -8,6 +8,9 @@ module dub.test.base;
 
 version (unittest):
 
+import std.array;
+public import std.algorithm;
+
 import dub.data.settings;
 public import dub.dependency;
 import dub.dub;
@@ -49,5 +52,73 @@ public class TestDub : Dub
     {
         // No-op
         return Settings.init;
+    }
+
+    /// See `MockPackageSupplier` documentation for this class' implementation
+    protected override PackageSupplier makePackageSupplier(string url) const
+    {
+        return new MockPackageSupplier(url);
+    }
+}
+
+/**
+ * Implements a `PackageSupplier` that doesn't do any IO
+ *
+ * This `PackageSupplier` needs to be pre-loaded with `Package` it can
+ * find during the setup phase of the unittest.
+ */
+public class MockPackageSupplier : PackageSupplier
+{
+    /// Mapping of package name to packages, ordered by `Version`
+    protected Package[][string] pkgs;
+
+    /// URL this was instantiated with
+    protected string url;
+
+    ///
+    public this(string url)
+    {
+        this.url = url;
+    }
+
+    ///
+    public override @property string description()
+    {
+        return "unittest PackageSupplier for: " ~ this.url;
+    }
+
+    ///
+    public override Version[] getVersions(string package_id)
+    {
+        if (auto ppkgs = package_id in this.pkgs)
+            return (*ppkgs).map!(pkg => pkg.version_).array;
+        return null;
+    }
+
+    ///
+    public override void fetchPackage(
+        NativePath path, string package_id, in VersionRange dep, bool pre_release)
+    {
+        assert(0, this.url ~ " - fetchPackage not implemented for: " ~ package_id);
+    }
+
+    ///
+    public override Json fetchPackageRecipe(
+        string package_id, in VersionRange dep, bool pre_release)
+    {
+        import dub.recipe.json;
+
+        if (auto ppkgs = package_id in this.pkgs)
+            foreach_reverse (pkg; *ppkgs)
+                if ((!pkg.version_.isPreRelease || pre_release) &&
+                    dep.matches(pkg.version_))
+                    return toJson(pkg.recipe);
+        return Json.init;
+    }
+
+    ///
+    public override SearchResult[] searchPackages(string query)
+    {
+        assert(0, this.url ~ " - searchPackages not implemented for: " ~ query);
     }
 }
