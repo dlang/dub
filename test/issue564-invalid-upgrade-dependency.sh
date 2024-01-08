482f5e3a1c78a770586890532dc6a4050ae16526
Mathias Lang <mlang@symmetryinvestments.com>
2024-01-08 23:41:28 +0100

    Fix #2691: Use same structure for managed and unmanaged path
    Currently, we issue a warning asking people to migrate, but the migrated
    path is not actually recognized. It was suggested in the issue to ignore
    this warning for add-path. However, as add-path is a rather low-level
    use, I think it is better to impose the same condition for managed and
    unmanaged path, in order to be able to provide the same guarantees.
    For example, once we move to reading version from the path,
    we can provide the same speedups for add-path users.
    We can also make sure that add-path users don't have surprises when
    dealing with packages that expect a certain structure outside their
    directory, such as arsd and ae.
    Some tests were updated with the new path, however, as many will likely
    be rewritten with the new test framework, and a lot of tests are affected,
    not all of them were updated, providing coverage for the old and new
    code alike.

--- test/issue564-invalid-upgrade-dependency.sh
+++ test/issue564-invalid-upgrade-dependency.sh
@@ -2,7 +2,4 @@
 
 . $(dirname "${BASH_SOURCE[0]}")/common.sh
 cd ${CURR_DIR}/issue564-invalid-upgrade-dependency
-rm -rf a-1.0.0/.dub
-rm -rf a-1.1.0/.dub
-rm -rf main/.dub
-${DUB} build --bare --compiler=${DC} main
+${DUB} build -f --bare --compiler=${DC} main
