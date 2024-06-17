08dcd477040cecb7e36094f080db6c75a15359ac
Mathias Lang <mlang@symmetryinvestments.com>
2024-06-17 14:46:05 +0200

    Improve tests

--- source/dub/test/selections_from_parent_dir.d
+++ source/dub/test/selections_from_parent_dir.d
@@ -1,3 +1,12 @@
+/*******************************************************************************
+
+    Test inheritable flag of selections file
+
+    Selections files can have an `inheritable` flag that is used to have
+    a central selections file, e.g. in the case of a monorepo.
+
+*******************************************************************************/
+
 module dub.test.selections_from_parent_dir;
 
 version (unittest):
@@ -45,7 +54,7 @@ dependency "pkg1" version="*"`);
     assert(dub.fs.existsFile(nestedPath));
     assert(dub.fs.readFile(path) == dubSelectionsJsonContent,
         "Inherited dub.selections.json modified after dub upgrade!");
-    const nestedContent = cast(string) dub.fs.readFile(nestedPath);
+    const nestedContent = dub.fs.readText(nestedPath);
     assert(nestedContent == dubSelectionsJsonContent.replace(`{"path":"pkg1"}`, `{"path":"../pkg1"}`),
         "Unexpected nestedContent:\n" ~ nestedContent);
 }
@@ -57,7 +66,7 @@ unittest
     const root_a = root ~ "a";
     const root_a_b = root_a ~ "b";
 
-    scope dub = new TestDub((scope FSEntry fs) {
+    scope dub_ = new TestDub((scope FSEntry fs) {
         // inheritable root/dub.selections.json
         fs.mkdir(root).writeFile(NativePath("dub.selections.json"), `{
 	"fileVersion": 1,
@@ -75,39 +84,65 @@ unittest
 	}
 }
 `);
-        // empty root/a/b/ directory
+        // We need packages for `loadPackage`
         fs.mkdir(root_a_b);
+        fs.writeFile(root_a_b ~ `dub.json`,
+            `{"name":"ab","dependencies":{"dub":"~>1.0"}}`);
+        fs.writeFile(root_a ~ `dub.json`,
+            `{"name":"a","dependencies":{"dub":"~>1.0"}}`);
+        fs.writeFile(root ~ `dub.json`,
+            `{"name":"r","dependencies":{"dub":"~>1.0"}}`);
+        fs.writePackageFile(`dub`, `1.37.0`, `{"name":"dub","version":"1.37.0"}`);
+        fs.writePackageFile(`dub`, `1.38.0`, `{"name":"dub","version":"1.38.0"}`);
     });
 
     // no selections for root/a/b/
     {
+        auto dub = dub_.newTest();
         const result = dub.packageManager.readSelections(root_a_b);
         assert(result.isNull());
+        dub.loadPackage(root_a_b);
+        assert(!dub.project.hasAllDependencies());
     }
 
     // local selections for root/a/
     {
+        auto dub = dub_.newTest();
         const result = dub.packageManager.readSelections(root_a);
         assert(!result.isNull());
         assert(result.get().absolutePath == root_a ~ "dub.selections.json");
         assert(!result.get().selectionsFile.inheritable);
+        dub.loadPackage(root_a);
+        assert(dub.project.hasAllDependencies());
+        assert(dub.project.dependencies()[0].name == "dub");
+        assert(dub.project.dependencies()[0].version_ == Version("1.37.0"));
     }
 
     // local selections for root/
     {
+        auto dub = dub_.newTest();
         const result = dub.packageManager.readSelections(root);
         assert(!result.isNull());
         assert(result.get().absolutePath == root ~ "dub.selections.json");
         assert(result.get().selectionsFile.inheritable);
+        dub.loadPackage(root);
+        assert(dub.project.hasAllDependencies());
+        assert(dub.project.dependencies()[0].name == "dub");
+        assert(dub.project.dependencies()[0].version_ == Version("1.38.0"));
     }
 
     // after removing non-inheritable root/a/dub.selections.json: inherited root selections for root/a/b/
     {
-        dub.fs.removeFile(root_a ~ "dub.selections.json");
-
+        auto dub = dub_.newTest((scope FSEntry fs) {
+            fs.removeFile(root_a ~ "dub.selections.json");
+        });
         const result = dub.packageManager.readSelections(root_a_b);
         assert(!result.isNull());
         assert(result.get().absolutePath == root ~ "dub.selections.json");
         assert(result.get().selectionsFile.inheritable);
+        dub.loadPackage(root_a_b);
+        assert(dub.project.hasAllDependencies());
+        assert(dub.project.dependencies()[0].name == "dub");
+        assert(dub.project.dependencies()[0].version_ == Version("1.38.0"));
     }
 }
