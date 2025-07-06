a3deb8ab5f1acedd655bcc78b605b40ac1057544
Andrei Horodniceanu <a.horodniceanu@proton.me>
2025-07-06 20:55:54 +0300

    Remove $DFLAGS build type (but keep supporting the $DFLAGS variable)
    While it is useful and expected that dub would honor the $DFLAGS
    variable, it should not drop *all* flags that it adds on its own.
    
    The most egregious way this is manifesting is that:
    - `dub test` - runs unittests (duh)
    - `DFLAGS= dub test` - does not run a single test
    
    This is because dub would treat the presence (even if empty) of
    $DFLAGS as flags to override the current buildType. This has been
    changed and now $DFLAGS simply are appended to the flags derived from
    the buildType. If users actually want to prevent dub from passing any
    special flags they can do so with `dub test --build=plain`
    
    Signed-off-by: Andrei Horodniceanu <a.horodniceanu@proton.me>

--- test/cache-generated-test-config.sh
+++ test/cache-generated-test-config.sh
@@ -31,15 +31,15 @@ STAT="stat -c '%Y'"
 
 EXECUTABLE_TIME="$(${STAT} cache-generated-test-config-test-library)"
 [ -z "$EXECUTABLE_TIME" ] && die $LINENO 'no EXECUTABLE_TIME was found'
-MAIN_TIME="$(${STAT} "$(ls $DUB_CODE_CACHE_PATH/*-\$DFLAGS-*/dub_test_root.d)")"
+MAIN_TIME="$(${STAT} "$(ls $DUB_CODE_CACHE_PATH/*/dub_test_root.d)")"
 [ -z "$MAIN_TIME" ] && die $LINENO 'no MAIN_TIME was found'
 
 DFLAGS="" ${DUB} test --compiler=${DC}
-MAIN_FILES_COUNT=$(ls $DUB_CODE_CACHE_PATH/*-\$DFLAGS-*/dub_test_root.d | wc -l)
+MAIN_FILES_COUNT=$(ls $DUB_CODE_CACHE_PATH/*/dub_test_root.d | wc -l)
 
 [ $MAIN_FILES_COUNT -ne 1 ] && die $LINENO 'DUB generated more then one main file'
 [ "$EXECUTABLE_TIME" != "$(${STAT} cache-generated-test-config-test-library)" ] && die $LINENO 'The executable has been rebuilt'
-[ "$MAIN_TIME" != "$(${STAT} "$(ls $DUB_CODE_CACHE_PATH/*-\$DFLAGS-*/dub_test_root.d | head -n1)")" ] && die $LINENO 'The test main file has been rebuilt'
+[ "$MAIN_TIME" != "$(${STAT} "$(ls $DUB_CODE_CACHE_PATH/*/dub_test_root.d | head -n1)")" ] && die $LINENO 'The test main file has been rebuilt'
 
 ## test with DFLAGS environment variable
 DFLAGS="-g" ${DUB} test --compiler=${DC}
@@ -49,15 +49,15 @@ STAT="stat -c '%Y'"
 
 EXECUTABLE_TIME="$(${STAT} cache-generated-test-config-test-library)"
 [ -z "$EXECUTABLE_TIME" ] && die $LINENO 'no EXECUTABLE_TIME was found'
-MAIN_TIME="$(${STAT} "$(ls $DUB_CODE_CACHE_PATH/*-\$DFLAGS-*/dub_test_root.d)")"
+MAIN_TIME="$(${STAT} "$(ls $DUB_CODE_CACHE_PATH/*/dub_test_root.d)")"
 [ -z "$MAIN_TIME" ] && die $LINENO 'no MAIN_TIME was found'
 
 DFLAGS="-g" ${DUB} test --compiler=${DC}
-MAIN_FILES_COUNT=$(ls $DUB_CODE_CACHE_PATH/*-\$DFLAGS-*/dub_test_root.d | wc -l)
+MAIN_FILES_COUNT=$(ls $DUB_CODE_CACHE_PATH/*/dub_test_root.d | wc -l)
 
 [ $MAIN_FILES_COUNT -ne 1 ] && die $LINENO 'DUB generated more then one main file'
 [ "$EXECUTABLE_TIME" != "$(${STAT} cache-generated-test-config-test-library)" ] && die $LINENO 'The executable has been rebuilt'
-[ "$MAIN_TIME" != "$(${STAT} "$(ls $DUB_CODE_CACHE_PATH/*-\$DFLAGS-*/dub_test_root.d | head -n1)")" ] && die $LINENO 'The test main file has been rebuilt'
+[ "$MAIN_TIME" != "$(${STAT} "$(ls $DUB_CODE_CACHE_PATH/*/dub_test_root.d | head -n1)")" ] && die $LINENO 'The test main file has been rebuilt'
 
 
 
