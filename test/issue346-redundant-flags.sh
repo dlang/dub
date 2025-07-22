d2a20744d3f274f6121558136eacc9ff0999516d
Andrei Horodniceanu <a.horodniceanu@proton.me>
2025-07-22 23:04:07 +0300

    issue346-redundant-flags
    Signed-off-by: Andrei Horodniceanu <a.horodniceanu@proton.me>

--- test/issue346-redundant-flags.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-. $(dirname "${BASH_SOURCE[0]}")/common.sh
-cd ${CURR_DIR}/issue346-redundant-flags
-${DUB} build --bare --force --compiler=${DC} -a x86_64 -v main 2>&1 | { ! grep -e '-m64 -m64' -c; }
