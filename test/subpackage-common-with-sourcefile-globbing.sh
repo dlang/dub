a9e4b987432fb730b3ec8b964053919859e59bde
Andrei Horodniceanu <a.horodniceanu@proton.me>
2025-07-26 16:16:03 +0300

    subpackage-common-with-sourcefile-globbing
    Signed-off-by: Andrei Horodniceanu <a.horodniceanu@proton.me>

--- test/subpackage-common-with-sourcefile-globbing.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-cd ${CURR_DIR}/subpackage-common-with-sourcefile-globbing
-rm -rf .dub dub.selections.json
-${DUB} build --compiler=${DC} :server -v
-${DUB} build --compiler=${DC} :client -v
-${DUB} build --compiler=${DC} :common -v
