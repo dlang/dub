0af444651c08398e655f1b2ec40a8acc2bf1099e
Andrei Horodniceanu <a.horodniceanu@proton.me>
2025-07-26 15:38:38 +0300

    issue990-download-optional-selected
    Signed-off-by: Andrei Horodniceanu <a.horodniceanu@proton.me>

--- test/issue990-download-optional-selected.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-. $(dirname "${BASH_SOURCE[0]}")/common.sh
-cd ${CURR_DIR}/issue990-download-optional-selected
-${DUB} clean
-${DUB} remove gitcompatibledubpackage -n 2>/dev/null || true
-${DUB} run
