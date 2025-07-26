8bb8fe663248db8ae819d0dfe3f60aa3d6fad292
Andrei Horodniceanu <a.horodniceanu@proton.me>
2025-07-26 22:14:01 +0300

    Drop test/version-filters.sh
    Signed-off-by: Andrei Horodniceanu <a.horodniceanu@proton.me>

--- test/version-filters.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-. $(dirname "${BASH_SOURCE[0]}")/common.sh
-
-$DUB build --root="$CURR_DIR/version-filters" --filter-versions
-$DUB build --root="$CURR_DIR/version-filters-diamond" --filter-versions
-$DUB build --root="$CURR_DIR/version-filters-source-dep" --filter-versions
-$DUB build --root="$CURR_DIR/version-filters-none" --filter-versions
