8bf0a62c5cf3628416b82c3ea68634dee9ba18f9
Vladimir Panteleev <git@cy.md>
2026-01-03 16:34:04 +0000

    ci(test/run-unittest): include test name in error messages

--- test/run-unittest.d
+++ test/run-unittest.d
@@ -71,7 +71,7 @@ int main(string[] args)
 			if (exists(min_frontend) && frontend.length && cmp(frontend, min_frontend.readText) < 0) continue;
 			log("Running " ~ script ~ "...");
 			if (spawnShell(script.name, ["DUB":dub, "DC":dc, "CURR_DIR":curr_dir]).wait)
-				logError("Script failure.");
+				logError(script.name.baseName, ": Script failure.");
 			else
 				log(script.name.baseName, " status: Ok");
 		}
@@ -86,7 +86,7 @@ int main(string[] args)
 		if (frontend.length && exists(min_frontend) && cmp(frontend, min_frontend.readText) < 0) continue;
 		log("Running " ~ script ~ "...");
 		if (spawnProcess([dub, script.name], ["DUB":dub, "DC":dc, "CURR_DIR":curr_dir]).wait)
-			logError("Script failure.");
+			logError(script.name.baseName, ": Script failure.");
 		else
 			log(script.name, " status: Ok");
 	}
@@ -123,13 +123,13 @@ int main(string[] args)
 				if (spawnProcess([dub, "build", "--force", "--compiler", dc], ["DUB":dub, "DC":dc, "CURR_DIR":curr_dir], ProcessConfig.none, pack.name).wait)
 					log(pack.name.baseName, " status: Ok");
 				else
-					logError("Failure expected, but build passed.");
+					logError(pack.name.baseName, ": Failure expected, but build passed.");
 			}
 			else
 			{
 				log("Building ", pack.name.baseName, "...");
 				if (spawnProcess([dub, "build", "--force", "--compiler", dc], ["DUB":dub, "DC":dc, "CURR_DIR":curr_dir], ProcessConfig.none, pack.name).wait)
-					logError("Script failure.");
+					logError(pack.name.baseName, ": Build failure.");
 				else
 					log(pack.name.baseName, " status: Ok");
 			}
@@ -150,7 +150,7 @@ int main(string[] args)
 		{
 			log("Running ", pack.name.baseName, "...");
 			if (spawnProcess([dub, "run", "--force", "--compiler", dc], ["DUB":dub, "DC":dc, "CURR_DIR":curr_dir], ProcessConfig.none, pack.name).wait)
-				logError("Run failure.");
+				logError(pack.name.baseName, ": Run failure.");
 			else
 				log(pack.name.baseName, " status: Ok");
 		}
@@ -167,7 +167,7 @@ int main(string[] args)
 		{
 			log("Testing ", pack.name.baseName, "...");
 			if (spawnProcess([dub, "test", "--force", "--root", pack.name, "--compiler", dc], ["DUB":dub, "DC":dc, "CURR_DIR":curr_dir]).wait)
-				logError("Test failure.");
+				logError(pack.name.baseName, ": Test failure.");
 			else
 				log(pack.name.baseName, " status: Ok");
 		}
