16dc07757ceb71ec16a93cb671e2a5f55a4fba2c
Mindy Batek <desisma@heidel.beer>
2026-09-05 05:05:44 +0200

    Skip incompatible tests on non-`x86_64`

--- test/issue686-multiple-march.sh
+++ test/issue686-multiple-march.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# FIXME: generalize test so it doesn't need to be skipped
+command -v arch >/dev/null \
+    && [ "$(arch)" != 'x86_64' ] \
+    && >&2 echo "Skipping test due to unsuitable arch." \
+    && exit 0
+
 . $(dirname "${BASH_SOURCE[0]}")/common.sh
 cd ${CURR_DIR}/issue686-multiple-march
 ${DUB} build --bare --force --compiler=${DC} -a x86_64 -v main 2>&1 | { ! grep -e '-m64 -m64' -c; }
